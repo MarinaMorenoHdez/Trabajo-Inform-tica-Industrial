c87bcd593375f337873d619e849c3009d0373e72
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:12:38 +0200

    Update alfil.cpp

--- AJEDREZ/src/alfil.cpp
+++ AJEDREZ/src/alfil.cpp
@@ -2,22 +2,8 @@
 
 vector<Vector2D> Alfil::movimientosPosibles(Pieza* tablero[10][8]) {
     vector<Vector2D> movs;
-    int x = posicion.x;
-    int y = posicion.y;
 
-    int dx[] = { 1, 1, -1, -1 };
-    int dy[] = { 1, -1, 1, -1 };
-
-    // Movimientos de alfil 
-    for (int dir = 0; dir < 4; ++dir) {
-        int x = posicion.x + dx[dir];
-        int y = posicion.y + dy[dir];
-        while (x >= 0 && x < 10 && y >= 0 && y < 8) {
-            movs.push_back(Vector2D(x, y));
-            if (tablero[x][y] != nullptr) break; // Puede comerse a sí mismo, pero no saltar
-            x += dx[dir];
-            y += dy[dir];
-        }
-    }
+    movimientosDiagonal(tablero, posicion, movs);
+    
     return movs;
 }
