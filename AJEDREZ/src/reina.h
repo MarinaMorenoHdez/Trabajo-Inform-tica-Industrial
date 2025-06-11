76945673fe0d5d73866e34ca4cb450fd3a15a1cd
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:16:54 +0200

    Update reina.h

--- AJEDREZ/src/reina.h
+++ AJEDREZ/src/reina.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
 class Reina : public Pieza
 {
 public:
 	Reina(int x, int y, char c) : Pieza(x, y, c, tipo::REINA) {} // Constructor 
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]) override;
 };
