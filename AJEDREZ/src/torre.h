f4c5826cf717f541e753ee11acd99a31de3ab8a3
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-11 16:46:00 +0200

    Delete AJEDREZ directory

--- AJEDREZ/src/torre.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pieza.h"
-class Torre : public Pieza
-{
-public:
-	Torre(int x, int y, char c) : Pieza(x, y, c, tipo::TORRE) {} // Constructor 
-	void mueve() override {}
-};
