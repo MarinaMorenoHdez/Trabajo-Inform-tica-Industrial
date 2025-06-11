5e31b6740118c1bbf5cd6e778d0ea4a6c7a8ae51
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:14:25 +0200

    Update caballo.h

--- AJEDREZ/src/caballo.h
+++ AJEDREZ/src/caballo.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
+using std::vector;
 class Caballo : public Pieza
 {
 public:
 	Caballo(int x, int y, char c) : Pieza(x, y, c, tipo::CABALLO) {} // Constructor 
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8])override;
 };
