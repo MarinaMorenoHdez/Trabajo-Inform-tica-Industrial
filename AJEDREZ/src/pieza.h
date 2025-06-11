00c9044b19c6117abfcab2b3fccef3ab8a6b4cbc
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:20:49 +0200

    Update pieza.h

--- AJEDREZ/src/pieza.h
+++ AJEDREZ/src/pieza.h
@@ -15,4 +15,8 @@ public:
 	Pieza(int x, int y, int c, tipo t) : color(c), posicion(x, y), t(t) {} // Constructor
 	virtual ~Pieza() = 0;
 	void mueve(Vector2D p) { posicion = p;}
+	virtual std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]) = 0; 
+	void movimientosHorVer(Pieza* tablero[10][8], const Vector2D& posicion, vector<Vector2D>& movs);
+	void movimientosCaballo(Pieza* tablero[10][8], const Vector2D& posicion, vector<Vector2D>& movs);
+	void movimientosDiagonal(Pieza* tablero[10][8], const Vector2D& posicion, vector<Vector2D>& movs);
 };
