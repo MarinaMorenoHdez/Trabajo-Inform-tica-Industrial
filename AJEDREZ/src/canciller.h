2a7dd79eab7cfa788155f0d0bfb7af1fc082cadb
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:15:02 +0200

    Update canciller.h

--- AJEDREZ/src/canciller.h
+++ AJEDREZ/src/canciller.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
 class Canciller : public Pieza
 {
 public:
 	Canciller(int x, int y, char c) : Pieza(x, y, c, tipo::CANCILLER) {} // Constructor 
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8])override;	
 };
