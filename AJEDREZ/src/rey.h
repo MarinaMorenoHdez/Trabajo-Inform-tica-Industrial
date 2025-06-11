938cc57970a9a9409cc1179477ad58d4a298cbe5
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:17:12 +0200

    Update rey.h

--- AJEDREZ/src/rey.h
+++ AJEDREZ/src/rey.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
 class Rey : public Pieza
 {
 public:
 	Rey(int x, int y, char c) : Pieza(x, y, c, tipo::REY) {} // Constructor 
-};
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]) override;
+}; 
