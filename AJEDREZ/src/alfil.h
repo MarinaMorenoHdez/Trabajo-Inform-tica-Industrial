2e4e8d2e7ea8622cd5062be3cdf1b35a9721e7cd
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-11 23:21:43 +0200

    Update alfil.h

--- AJEDREZ/src/alfil.h
+++ AJEDREZ/src/alfil.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
+using std::vector;
 class Alfil : public Pieza
 {
 public:
-	Alfil(int x, int y, char c) : Pieza(x, y, c, tipo::ALFIL) {} // Constructor 
+	Alfil(int x, int y, char c) : Pieza(x, y, c, tipo::ALFIL) {} // Constructor
+
 	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]);
 };
-
 
