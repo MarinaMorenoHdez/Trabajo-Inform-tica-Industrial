e34174e25498c978dccadc65e1660306663dd729
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-11 21:51:51 +0200

    Update peon.cpp

--- AJEDREZ/src/peon.cpp
+++ AJEDREZ/src/peon.cpp
@@ -1,24 +1,34 @@
 #include "peon.h"
 
 
-vector <Vector2D> Peon::getMovimientos(Peon& p) {
-	vector <Vector2D> movimientos;
-	if (color == 'B') { // Peón blanco
+vector<Vector2D> Peon::movimientosPosibles(Pieza* tablero[10][8]) {
+    vector<Vector2D> movs;
+    int dir = (color == 'B') ? 1 : -1;
+    int x = posicion.x;
+    int y = posicion.y;
 
-		movimientos[0] = (posicion.x, posicion.y + 2);
-		movimientos[1] = (posicion.x, posicion.y + 1);
-		movimientos[2] = (posicion.x + 1, posicion.y + 1);
-		movimientos[3] = (posicion.x - 1, posicion.y + 1);
-	}
-	if (color == 'N') { // Peón negro
+    // Movimiento hacia adelante
+    if (y + dir >= 0 && y + dir < 8 && tablero[x][y + dir] == nullptr)
+        movs.push_back(Vector2D(x, y + dir));
 
-		movimientos[0] = (posicion.x, posicion.y - 2);
-		movimientos[1] = (posicion.x, posicion.y - 1);
-		movimientos[2] = (posicion.x + 1, posicion.y - 1);
-		movimientos[3] = (posicion.x - 1, posicion.y - 1);
-	}
+	// Primer movimiento doble hacia adelante 
+    int filaInicial = (color == 'B') ? 1 : 6;
+    if (y == filaInicial &&
+        y + dir >= 0 && y + dir < 8 && tablero[x][y + dir] == nullptr &&
+        y + 2 * dir >= 0 && y + 2 * dir < 8 && tablero[x][y + 2 * dir] == nullptr)
+    {
+        movs.push_back(Vector2D(x, y + 2 * dir));
+    }
 
-	return movimientos;
+    // Captura a la izquierda 
+    if (x - 1 >= 0 && y + dir >= 0 && y + dir < 8 && tablero[x - 1][y + dir] != nullptr)
+        movs.push_back(Vector2D(x - 1, y + dir));
+
+    // Captura a la derecha 
+    if (x + 1 < 10 && y + dir >= 0 && y + dir < 8 && tablero[x + 1][y + dir] != nullptr)
+        movs.push_back(Vector2D(x + 1, y + dir));
+
+    return movs;
 }
 
 void Peon::mueve(Vector2D p)
