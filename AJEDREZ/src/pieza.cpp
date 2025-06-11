eb6bad7506a418e14f3b3970484e2fbe556cfef6
MiguelOlallaT <miguel.olalla@alumnos.upm.es>
2025-06-11 15:59:54 +0200

    Update pieza.cpp

--- AJEDREZ/src/pieza.cpp
+++ AJEDREZ/src/pieza.cpp
@@ -1,1 +1,28 @@
-#include "pieza.h"
+#include "peon.h"
+
+
+vector <Vector2D> Peon::getMovimientos(Peon& p) {
+	vector <Vector2D> movimientos;
+	if (color == 'B') { // Peón blanco
+
+		movimientos[0] = (posicion.x, posicion.y + 2);
+		movimientos[1] = (posicion.x, posicion.y + 1);
+		movimientos[2] = (posicion.x + 1, posicion.y + 1);
+		movimientos[3] = (posicion.x - 1, posicion.y + 1);
+	}
+	if (color == 'N') { // Peón negro
+
+		movimientos[0] = (posicion.x, posicion.y - 2);
+		movimientos[1] = (posicion.x, posicion.y - 1);
+		movimientos[2] = (posicion.x + 1, posicion.y - 1);
+		movimientos[3] = (posicion.x - 1, posicion.y - 1);
+	}
+
+	return movimientos;
+}
+
+void Peon::mover(int x, int y)
+{
+	posicion.x = x;
+	posicion.y = y;
+}
