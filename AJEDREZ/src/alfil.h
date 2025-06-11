d51c40e800d8d6182bbed5a0a8d09d709928d7b6
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-11 23:21:09 +0200

    Update alfil.h

--- AJEDREZ/src/alfil.h
+++ AJEDREZ/src/alfil.h
@@ -4,6 +4,7 @@ class Alfil : public Pieza
 {
 public:
 	Alfil(int x, int y, char c) : Pieza(x, y, c, tipo::ALFIL) {} // Constructor 
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]);
 };
 
 
