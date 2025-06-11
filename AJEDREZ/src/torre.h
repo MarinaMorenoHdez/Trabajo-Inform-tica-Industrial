751a24327f8770e41aabce1ece248d31fb4f835c
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 01:18:33 +0200

    Update torre.h

--- AJEDREZ/src/torre.h
+++ AJEDREZ/src/torre.h
@@ -1,7 +1,10 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
 class Torre : public Pieza
 {
 public:
 	Torre(int x, int y, char c) : Pieza(x, y, c, tipo::TORRE) {} // Constructor 
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]) override;
 };
