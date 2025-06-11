ef282e0e0d89e4239a8e47b69267b34cdc1dbb02
MarinaMorenoHdez <marina.moreno@alumnos.upm.es>
2025-06-12 00:27:01 +0200

    Update arzobispo.h

--- AJEDREZ/src/arzobispo.h
+++ AJEDREZ/src/arzobispo.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "pieza.h"
+#include "vector2D.h"
+#include <vector>
+using std::vector;
+
 class Arzobispo : public Pieza
 {
 public:
 	Arzobispo(int x, int y, char c) : Pieza(x, y, c, tipo::ARZOBISPO) {} // Constructor 
+
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]);
 };
