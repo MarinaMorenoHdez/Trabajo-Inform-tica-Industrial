0cd9ea4febebdea4a3a2f67b1c8a33d208d3633c
lauraasanchez <laura.sanchez.munoz@alumnos.upm.es>
2025-06-12 22:08:26 +0200

    Update canciller.cpp

--- AJEDREZ/src/canciller.cpp
+++ AJEDREZ/src/canciller.cpp
@@ -1,4 +1,7 @@
 #include "canciller.h"
+#include "freeglut.h"
+#include <ETSIDI.h>
+#include <string>
 
 // Torre y Caballo
 std::vector<Vector2D> Canciller::movimientosPosibles(Pieza* tablero[10][8])
@@ -9,4 +12,17 @@ std::vector<Vector2D> Canciller::movimientosPosibles(Pieza* tablero[10][8])
 	movimientosCaballo(tablero, posicion, movs);
 
     return movs;
+}
+void Canciller::dibuja() {
+    std::string ruta = (color == 'B') ? "imagenes/cancillerrojo.png" : "imagenes/cancillerazules.png";
+    ETSIDI::Sprite sprite(ruta.c_str());
+    sprite.setSize(4, 4);  
+
+    float x = posicion.x * 4.0f;  
+    float y = posicion.y * 4.0f;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0.1f);  
+    sprite.draw();
+    glPopMatrix();
 }
