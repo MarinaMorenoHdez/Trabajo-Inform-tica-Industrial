dd6a632e4c9b3e7491387e78f46f67d54465ceab
lauraasanchez <laura.sanchez.munoz@alumnos.upm.es>
2025-06-12 22:11:07 +0200

    Update reina.h

--- AJEDREZ/src/reina.h
+++ AJEDREZ/src/reina.h
@@ -7,4 +7,5 @@ class Reina : public Pieza
 public:
 	Reina(int x, int y, char c) : Pieza(x, y, c, tipo::REINA) {} // Constructor 
 	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8]) override;
+	void dibuja();
 };
