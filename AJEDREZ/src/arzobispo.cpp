3d2598a0ab6c26e74b5db7b6f781505c3697c70f
lauraasanchez <laura.sanchez.munoz@alumnos.upm.es>
2025-06-12 22:06:10 +0200

    Update arzobispo.cpp

--- AJEDREZ/src/arzobispo.cpp
+++ AJEDREZ/src/arzobispo.cpp
@@ -1,4 +1,7 @@
 #include "arzobispo.h"
+#include "freeglut.h"
+#include <ETSIDI.h>
+#include <string>
 
 // Alfil y Caballo
 std::vector<Vector2D> Arzobispo::movimientosPosibles(Pieza* tablero[10][8])
@@ -9,4 +12,18 @@ std::vector<Vector2D> Arzobispo::movimientosPosibles(Pieza* tablero[10][8])
 	movimientosCaballo(tablero, posicion, movs);
 
     return movs;
+}
+
+void Arzobispo::dibuja() {
+    std::string ruta = (color == 'B') ? "imagenes/arzobisporojo.png" : "imagenes/arzobispoazules.png";
+    ETSIDI::Sprite sprite(ruta.c_str());
+    sprite.setSize(4, 4); 
+
+    float x = posicion.x * 4.0f;  
+    float y = posicion.y * 4.0f;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0.1f); 
+    sprite.draw();
+    glPopMatrix();
 }
