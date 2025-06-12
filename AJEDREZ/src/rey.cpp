72749239877df5c2ea9adc5d3f9c79de1d28da65
lauraasanchez <laura.sanchez.munoz@alumnos.upm.es>
2025-06-12 22:11:49 +0200

    Update rey.cpp

--- AJEDREZ/src/rey.cpp
+++ AJEDREZ/src/rey.cpp
@@ -1,4 +1,7 @@
 #include "rey.h"
+#include "freeglut.h"
+#include <ETSIDI.h>
+#include <string>
 
 
 std::vector<Vector2D> Rey::movimientosPosibles(Pieza* tablero[10][8])
@@ -15,4 +18,17 @@ std::vector<Vector2D> Rey::movimientosPosibles(Pieza* tablero[10][8])
         }
     }
     return movs;
+}
+void Rey::dibuja() {
+    std::string ruta = (color == 'B') ? "imagenes/reyrojo.png" : "imagenes/reyazules.png";
+    ETSIDI::Sprite sprite(ruta.c_str());
+    sprite.setSize(4, 4);  
+
+    float x = posicion.x * 4.0f;  
+    float y = posicion.y * 4.0f;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0.1f);  
+    sprite.draw();
+    glPopMatrix();
 }
