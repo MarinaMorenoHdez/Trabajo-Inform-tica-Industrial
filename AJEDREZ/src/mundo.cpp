04fd84569d92e3ea76b1d8cd90d8e825fc529626
albamfernandez <alba.mfernandez@alumnos.upm.es>
2025-06-12 23:05:28 +0200

    Update mundo.cpp

--- AJEDREZ/src/mundo.cpp
+++ AJEDREZ/src/mundo.cpp
@@ -4,23 +4,15 @@
 #include <ETSIDI.h>
 #include "tablero.h"
 
-using namespace std;
-void Mundo::inicializa(int tipo_juego, const int& tipo_oponente)
-{
-
-	
-
-}
-void Mundo::dibuja()
-{
-	// dibujo de elementos y avisos
-	tablero.dibuja();
-
+void Mundo::inicializa(int tipo_juego, const int& tipo_oponente) {
+    this->tipo_oponente = tipo_oponente;
+   
 }
 
-void Mundo::MouseButton(int tipo_oponente, int x, int y, int boton, bool abajo, bool TeclaSp, bool TeclaCtr)
-{
+void Mundo::dibuja() {
+    tablero.dibuja();
+}
 
-	
-
+void Mundo::MouseButton(int tipo_oponente, int x, int y, int boton, bool abajo, bool TeclaSp, bool TeclaCtr) {
+   
 }
