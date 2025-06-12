7b99bb5625b0f5f1c2e368a3e05b09ccfbe17043
lauraasanchez <laura.sanchez.munoz@alumnos.upm.es>
2025-06-12 22:09:42 +0200

    Update peon.cpp

--- AJEDREZ/src/peon.cpp
+++ AJEDREZ/src/peon.cpp
@@ -1,4 +1,7 @@
 #include "peon.h"
+#include "freeglut.h"
+#include <ETSIDI.h>
+#include <string>
 
 
 vector<Vector2D> Peon::movimientosPosibles(Pieza* tablero[10][8]) {
@@ -29,5 +32,19 @@ vector<Vector2D> Peon::movimientosPosibles(Pieza* tablero[10][8]) {
         movs.push_back(Vector2D(x + 1, y + dir));
 
     return movs;
+}
+
+void Peon::dibuja() {
+    std::string ruta = (color == 'B') ? "imagenes/peonrojo.png" : "imagenes/peonazules.png";
+    ETSIDI::Sprite sprite(ruta.c_str());
+    sprite.setSize(4, 4);  
+
+    float x = posicion.x * 4.0f; 
+    float y = posicion.y * 4.0f;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0.1f);  
+    sprite.draw();
+    glPopMatrix();
 }
 
