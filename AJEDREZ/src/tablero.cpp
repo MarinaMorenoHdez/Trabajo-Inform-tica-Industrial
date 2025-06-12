a418877d02638b116d24f3cddde71b31577b1892
paulavr04 <paula.vrodriguez@alumnos.upm.es>
2025-06-12 11:19:10 +0200

    Update tablero.cpp

--- AJEDREZ/src/tablero.cpp
+++ AJEDREZ/src/tablero.cpp
@@ -37,7 +37,7 @@ bool Tablero::moverPieza(Vector2D origen, Vector2D destino) {
 	if (!valido) return false;
 	//captura de la pieza contraria, eliminarla de lista de piezas
 	Pieza* destinoPieza = tablero[destino.x][destino.y]; //ver si hay una pieza en la casilla destino
-	if (destinoPieza != nullptr && destinoPieza->getColor() != turno) { //comprobar si hay pieza y es de color contrario
+	if (destinoPieza != nullptr) { //comprobar si hay pieza para comer
 	// Eliminar del vector de piezas
 	auto it = std::find(piezas.begin(), piezas.end(), destinoPieza);
 	if (it != piezas.end()) piezas.erase(it);
