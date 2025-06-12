bd7cc20b204da404dd1ec7553c43a585d33a1726
albamfernandez <alba.mfernandez@alumnos.upm.es>
2025-06-12 23:37:55 +0200

    Update mundo.h

--- AJEDREZ/src/mundo.h
+++ AJEDREZ/src/mundo.h
@@ -7,9 +7,9 @@ class Mundo {
 
 public:
     int get_oponente() const { return tipo_oponente; }
+    Tablero& getTablero() { return tablero; } // Getter para acceder a tablero desde control
 
     void inicializa(int tipo_juego, const int& tipo_oponente);
     void dibuja();
     void MouseButton(int tipo_oponente, int x, int y, int boton, bool abajo, bool TeclaSp, bool TeclaCtr);
 };
-
