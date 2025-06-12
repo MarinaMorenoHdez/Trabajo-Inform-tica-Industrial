bff9b4bb84840451de08758e82899dda27b41f8e
lauraasanchez <laura.sanchez.munoz@alumnos.upm.es>
2025-06-12 22:10:46 +0200

    Update reina.cpp

--- AJEDREZ/src/reina.cpp
+++ AJEDREZ/src/reina.cpp
@@ -1,4 +1,7 @@
 #include "reina.h"
+#include "freeglut.h"
+#include <ETSIDI.h>
+#include <string>
 
 std::vector<Vector2D> Reina::movimientosPosibles(Pieza* tablero[10][8])
 {
@@ -8,4 +11,17 @@ std::vector<Vector2D> Reina::movimientosPosibles(Pieza* tablero[10][8])
 	movimientosDiagonal(tablero, posicion, movs);
 
     return movs;
+}
+void Reina::dibuja() {
+    std::string ruta = (color == 'B') ? "imagenes/reinarojo.png" : "imagenes/reinaazules.png";
+    ETSIDI::Sprite sprite(ruta.c_str());
+    sprite.setSize(4, 4);  
+
+    float x = posicion.x * 4.0f; 
+    float y = posicion.y * 4.0f;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0.1f); 
+    sprite.draw();
+    glPopMatrix();
 }
