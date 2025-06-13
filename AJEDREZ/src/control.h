eb905ab4117b4ea63478be4a1db3c30658544d90
albamfernandez <alba.mfernandez@alumnos.upm.es>
2025-06-13 19:41:35 +0200

    Update control.h

--- AJEDREZ/src/control.h
+++ AJEDREZ/src/control.h
@@ -4,6 +4,8 @@
 #include "tablero.h"
 #include "mundo.h"
 
+
+
 enum
 {
 	JUEGO = 0, INICIO, START,
@@ -17,11 +19,27 @@ class control {
 	int estado = 1;
 	Mundo mundo;
 
+private:
+private:
+	Vector2D casillaSeleccionada = { -1, -1 };  // Inicialmente inválida
+	bool piezaSeleccionada = false;
+	Vector2D seleccion;
+	void gestionarMovimientoJugador(Vector2D coord);
+
+
 public:
 	void tecla(unsigned char key);
 	void dibuja();
 	void MouseButton(int tipo_oponente, int x, int y, int boton, bool abajo, bool TeclaSp, bool TeclaCtr);
 	int Get_Estado() { return estado; }
 	Vector2D mouseToBoardCoords(int x, int y);
-};
+	
+	Vector2D getCasillaSeleccionada() const { return casillaSeleccionada; }
+	bool haySeleccion() const { return piezaSeleccionada; }
 
+
+	control();
+
+
+
+};
