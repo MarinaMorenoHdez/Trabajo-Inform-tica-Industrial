3c7565dfd6d5301f18f60735750b3ef0764face8
albamfernandez <alba.mfernandez@alumnos.upm.es>
2025-06-13 19:43:05 +0200

    Update mundo.cpp

--- AJEDREZ/src/mundo.cpp
+++ AJEDREZ/src/mundo.cpp
@@ -1,12 +1,17 @@
 #include "mundo.h"
-#include <iostream>
 #include "freeglut.h"
 #include <ETSIDI.h>
-#include "tablero.h"
+#include <iostream>
+
+// Este método se llama desde control para pasarle el puntero a sí mismo
+void Mundo::setControl(control* c) {
+    refControl = c;
+    tablero.setControl(c);  // También se lo pasamos al tablero
+}
 
 void Mundo::inicializa(int tipo_juego, const int& tipo_oponente) {
     this->tipo_oponente = tipo_oponente;
-    // Puedes agregar lógica de inicialización aquí
+    // Lógica adicional si hace falta
 }
 
 void Mundo::dibuja() {
@@ -14,6 +19,6 @@ void Mundo::dibuja() {
 }
 
 void Mundo::MouseButton(int tipo_oponente, int x, int y, int boton, bool abajo, bool TeclaSp, bool TeclaCtr) {
-    // Lógica del ratón en el modo JUEGO, si aplica
+    // Este método está vacío si no necesitas usarlo directamente
 }
 
