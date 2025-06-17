7bb8e46054dd239b4932893fb5c3e73c9ad15665
paulavr04 <paula.vrodriguez@alumnos.upm.es>
2025-06-17 17:28:43 +0200

    Update reina.cpp

--- AJEDREZ/src/reina.cpp
+++ AJEDREZ/src/reina.cpp
@@ -3,7 +3,7 @@
 #include <ETSIDI.h>
 #include <string>
 
-std::vector<Vector2D> Reina::movimientosPosibles(Pieza* tablero[10][8])
+std::vector<Vector2D> Reina::movimientosPosibles(Pieza* tablero[10][8],Tablero* /*refTablero*/)
 {
     std::vector<Vector2D> movs;
 
