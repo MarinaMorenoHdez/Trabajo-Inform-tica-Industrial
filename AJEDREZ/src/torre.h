2cc14f7373e2d44c389cdd0e740de57d1b07dccc
paulavr04 <paula.vrodriguez@alumnos.upm.es>
2025-06-17 17:27:06 +0200

    Update torre.h

--- AJEDREZ/src/torre.h
+++ AJEDREZ/src/torre.h
@@ -6,6 +6,6 @@ class Torre : public Pieza
 {
 public:
 	Torre(int x, int y, char c) : Pieza(x, y, c, tipo::TORRE) {} // Constructor 
-	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8],/*refTablero*/) override;
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8],Tablero* /*refTablero*/) override;
 	void dibuja();
 };
