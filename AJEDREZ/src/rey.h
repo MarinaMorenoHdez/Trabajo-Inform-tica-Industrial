26b4ab18a639c379b9a8b1f58f91f2eaf4211c62
paulavr04 <paula.vrodriguez@alumnos.upm.es>
2025-06-17 17:28:01 +0200

    Update rey.h

--- AJEDREZ/src/rey.h
+++ AJEDREZ/src/rey.h
@@ -6,6 +6,6 @@ class Rey : public Pieza
 {
 public:
 	Rey(int x, int y, char c) : Pieza(x, y, c, tipo::REY) {} // Constructor 
-	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8],Tablero* refTabler) override;
+	std::vector<Vector2D> movimientosPosibles(Pieza* tablero[10][8],Tablero* refTablero) override;
 	void dibuja();
 }; 
